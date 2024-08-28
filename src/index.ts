961b2d0bb71d31a52040ea1fc2ced2362d56c8d3
Tayllana Wislla <tayllanawislla@gmail.com>
2024-08-28 14:20:38 +0000

    chore: Update port number to 3000 in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import express from 'express'
-// import cors from 'cors'
 import { connect } from './database'
 
-const port = 3333
+const port = 3000
 const app = express()
 
-// app.use(cors())
 app.use(express.json())
 app.use(express.static(__dirname + '/../public'))
-
-app.get('/', async (req, res) => {
-    res.sendFile("Hello World");
-  })
 
 app.get('/users', async (req, res) => {
   const db = await connect()
